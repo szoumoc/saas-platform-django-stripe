39f2b35fb3b0298827434216eaca2b837ccc6e34
szoumoc <soumosaha4327@gmail.com>
2025-06-16 13:35:23 +0530

    updated views to change order

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,12 +9,13 @@ def home_page_view(request, *args, **kwargs ):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path = request.path)
     my_title = "My page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": qs.count(),
         "total_visit_count": page_qs.count()
     }
     path = request.path
-    html_template = "home.html"
+    
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
