876f0f09b3d70406a2354c0408862fd42da282d8
szoumoc <soumosaha4327@gmail.com>
2025-07-04 15:23:25 +0530

    Added Groups and permissions

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<username>/", views.profile_view, name="profile"),
+
+    path("", views.profile_list_view),
+    path("<str:username>/", views.profile_detail_view),
 
 ]
   
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -6,12 +6,37 @@ from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
+
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_list_view(request, *args, **kwargs):
+    context = {
+        "object_list": User.objects.filter(is_active=True).order_by("-date_joined"),
+        
+    }
+    return render(request, "profiles/list.html", context)
+
+
+
+
+
+@login_required
+def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
+    print(
+        user.has_perm("subscriptions.basic"),
+        user.has_perm("subscriptions.basic_ai"),
+        user.has_perm("subscriptions.pro"),
+        user.has_perm("subscriptions.advanced"),    
+    )
+    # user_groups = user.groups.all()
+    # print("user_groups", user_groups)
+    # if user_groups.filter(name__icontains='basic').exists():
+    #     return HttpResponse("Congrats")
     profile_user_obj = get_object_or_404(User, username=username)
-    print(profile_user_obj)
-    print(user)
     is_me = profile_user_obj == user
-    return HttpResponse(f"Profile View for {profile_user_obj.id}, is_me: {is_me}")
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    return render(request, "profiles/detail.html", context)
